d97c616a862dab231040f641ef589e168df52dab
Conrado Cañas <conradocanas@gmail.com>
2020-08-14 01:08:32 -0300

    First upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const nombre = "Conrado Cañas"
+const experiencia = [{
+    lugar: "Freelancer",
+    periodo: "2019-2020",
+    descripción: "Montaje de sitios web"
+    },
+    {
+    lugar: "Alto Brillo S.A.",
+    periodo: "2012-2018",
+    descripción: "Gerente de marca"
+    },{
+    lugar: "Rio Yuspe SRL",
+    periodo: "2011",
+    descripción: "Venta minorista y caja."
+    }
+];
+const estudios = [{
+    lugar: "Saul A. Taborda",
+    periodo: "2011-2018",
+    descripción: "Bachiller en Ciencias Naturales y Medio Ambiente"
+    }
+];
+
+function init() {
+    // Cambia el nombre del H1 que con tiene el nombre del usuario
+    var nombreDOM = document.getElementById("name")
+    nombreDOM.innerText = nombre
+
+    // Itineración que crea los items en "Experiencia"
+    for (i = 0; i < experiencia.length; i++){
+        var item = document.createElement("div")
+        item.innerHTML =
+        `<div class="d-flex justify-content-between">
+        <span class="card-title h6 exp-title">${experiencia[i].lugar}</span>
+        <span class="exp-date">${experiencia[i].periodo}</span>
+        </div>
+        <p>${experiencia[i].descripción}</p>`
+        document.getElementById("experiencia").appendChild(item)
+    }
+
+    // Itineración que crea los items en "Estudios"
+    for (i = 0; i < estudios.length; i++){
+        var item = document.createElement("div")
+        item.innerHTML =
+        `<div class="d-flex justify-content-between">
+        <span class="card-title h6 exp-title">${estudios[i].lugar}</span>
+        <span class="exp-date">${estudios[i].periodo}</span>
+        </div>
+        <p>${estudios[i].descripción}</p>`
+        document.getElementById("estudios").appendChild(item)
+    }
+
+}
+
+init()
