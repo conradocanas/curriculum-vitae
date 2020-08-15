9a79a834e12ee923a9ab8847eb801bb4841b8a7c
Conrado Cañas <conradocanas@gmail.com>
2020-08-15 02:01:05 -0300

    Agregadas secciones de lenguajes y certificados

--- main.js
+++ main.js
@@ -20,6 +20,28 @@ const estudios = [{
     descripción: "Bachiller en Ciencias Naturales y Medio Ambiente"
     }
 ];
+const lenguajesActuales =
+[
+    "https://devicons.github.io/devicon/devicon.git/icons/javascript/javascript-plain.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/html5/html5-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/css3/css3-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/bootstrap/bootstrap-plain.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/visualstudio/visualstudio-plain.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/github/github-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/wordpress/wordpress-plain.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/slack/slack-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/photoshop/photoshop-plain.svg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Microsoft_Excel_2013_logo.svg/782px-Microsoft_Excel_2013_logo.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Microsoft_Word_2013_logo.svg/611px-Microsoft_Word_2013_logo.svg.png"
+]
+const lenguajesAprendiendo =
+[
+    "https://devicons.github.io/devicon/devicon.git/icons/python/python-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/vuejs/vuejs-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/angularjs/angularjs-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/nodejs/nodejs-original.svg",
+    "https://devicons.github.io/devicon/devicon.git/icons/mongodb/mongodb-original.svg"
+]
 
 function init() {
     // Cambia el nombre del H1 que con tiene el nombre del usuario
@@ -48,6 +70,20 @@ function init() {
         </div>
         <p>${estudios[i].descripción}</p>`
         document.getElementById("estudios").appendChild(item)
+    }
+
+    // Itineración que crea los items en "Lenguajes y programas"
+    for (i = 0; i < lenguajesActuales.length; i++){
+        var item = document.createElement("div")
+        item.innerHTML = `<img style="margin-right: 15px" align="left" alt="JavaScript" width="40" src="${lenguajesActuales[i]}" />`
+        document.getElementById("lenguajesActuales").appendChild(item)
+    }
+
+    // Itineración que crea los items en "Lenguajes y tecnologias aprendiendo"
+    for (i = 0; i < lenguajesAprendiendo.length; i++){
+        var item = document.createElement("div")
+        item.innerHTML = `<img style="margin-right: 15px" align="left" alt="JavaScript" width="40" src="${lenguajesAprendiendo[i]}" />`
+        document.getElementById("lenguajesAprendiendo").appendChild(item)
     }
 
 }
